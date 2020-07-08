7d6c08529bb7a6a765a1644170db58eaa048c739
Dulmi Chaya <cdulmei@yahoo.com>
2020-07-08 07:53:48 +0530

    Added initial project structure - backend

---
+++ SourceCode/OnlineStore/OnlineStore.DataAccess/DataContract/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlineStore.DataAccess.DataContract
+{
+    public class DatabaseContext:DbContext
+    {
+        public class OptionBuild
+        {
+            public DbContextOptionsBuilder<DatabaseContext> opsBuilder { get; set; }
+            public DbContextOptions<DatabaseContext> MyProperty { get; set; }
+            private AppConfiguration settings { get; set; }
+        }
+    }
+}
